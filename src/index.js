5e1c87ca1a204b34dd22c81c41626631addff340
Rishabh <rishabh31000@gmail.com>
2021-06-29 22:31:01 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {HashRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
+
 
 
 ReactDOM.render(
   
+  
   <React.StrictMode>
    
     
-     
+     <HashRouter>
       <App />
+     </HashRouter>
+      
     
     
     
