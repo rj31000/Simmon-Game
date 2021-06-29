152866df0d40872a0bbcb330b2036637febaffbe
Rishabh <rishabh31000@gmail.com>
2021-06-29 23:00:01 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 import Heading from './components/Heading';
 import Game from './components/Game';
 import { Route, BrowserRouter } from 'react-router-dom';
-
+import {HashRouter} from 'react-router-dom'
 function App() {
   
   return (
     <div className="App min-h-screen bg-red-500">
       
-      <BrowserRouter>
+      <HashRouter>
       <Route path="/" exact component={Heading} />
       <Route path="/Game" exact component={Game} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
